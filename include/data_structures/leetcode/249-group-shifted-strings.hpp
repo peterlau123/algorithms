cfb924cdf0008534c7b8cd9ddd8f61b3badb257c
liuxin.image <liuxin.image@bytedance.com>
2025-03-30 23:00:36 +0800

    feat: add leetcode problems

---
+++ include/data_structures/leetcode/249-group-shifted-strings.hpp
@@ -0,0 +1,12 @@
+#include "base/cpp_standard_header.h"
+
+namespace leetcode::group_shifted_strings {
+
+    class Solution {
+        public:
+            //同一个group的string，string之间的距离对于每个char是一致的
+            vector<vector<string>> groupStrings(vector<string>& strings) {
+                
+            }
+        };
+}
